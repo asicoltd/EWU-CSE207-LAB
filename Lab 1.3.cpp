6ff90cb9c7189776dbd36d2967ee21cb3332063e
asicoltd <61615468+asicoltd@users.noreply.github.com>
2021-07-01 00:18:58 +0600

    Update Lab 1.3.cpp

--- Lab 1.3.cpp
+++ Lab 1.3.cpp
@@ -1,31 +1,14 @@
 #include<iostream>
 using namespace std;
-struct prime{
+struct prime{           
     int range1;
     int range2;
     int counter;
-    int *num;
+    int *num;       
 };
-int* primeCatcher(prime p);
-int main(){
-prime prm;
-
-cout << "First number: ";
-cin >> prm.range1;
-cout << "Last number: ";
-cin >> prm.range2;
-int* primeNumbers = primeCatcher(prm);
-for( int i = 0 ; ; i++ ){
-    if( *(primeNumbers+i) < prm.range1 || *(primeNumbers+i) > prm.range2 )break;
-    cout << *(primeNumbers+i) << endl;
-
-}
-delete []primeNumbers;
-return 0;
-}
-int* primeCatcher(prime p){
+int* primeCatcher(prime p){        // this function will find the prime from struct and return a pointer
     p.counter = 0;
-    p.num = new int[p.range2-p.range1];
+    p.num = new int[p.range2-p.range1];         // p.num is the array where we will add the prime numbers
     for( int i = p.range1 ; i < p.range2 ;i++){
         int flag = 0;
         for( int j = 2 ; j < i ; j++ ){
@@ -35,9 +18,27 @@ int* primeCatcher(prime p){
             }
         }
         if( flag == 0 ){
-            p.num[p.counter] = i;
-            p.counter++;
+            p.num[p.counter] = i;       // adding prime numbers in p.num[]
+            p.counter++;                // p.counter will count the total number of prime 
         }
     }
-    return p.num;
+    return p.num;       // returning the address of the array 
+}
+
+int main(){
+prime prm;
+    
+cout << "First number: ";
+cin >> prm.range1;
+cout << "Last number: ";
+cin >> prm.range2;
+
+int* primeNumbers = primeCatcher(prm); // using the function
+for( int i = 0 ; ; i++ ){
+    if( *(primeNumbers+i) < prm.range1 || *(primeNumbers+i) > prm.range2 )break; //only values between range1 and range2 will be printed
+    cout << *(primeNumbers+i) << endl; //printing the prime numbers
+
+}
+delete []primeNumbers; //deleting the dynamic location
+return 0;
 }
